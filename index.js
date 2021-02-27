18b06108cdb5194bdac34ace0c84f4d7f7bc2142
Lárisson Aquino <larissonaquino@gmail.com>
2021-02-27 19:29:23 -0300

    error handle in mongo connection

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cdz-ultimate.biz00.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+}).catch(e => {
+    console.error('error in connect to MongoDB', e)
 });
 
 app.use(cors());
