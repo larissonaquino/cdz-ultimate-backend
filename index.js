93a967eb25a4d1b0a3d4980751ba6d214e589460
Lárisson Aquino <larissonaquino@gmail.com>
2021-04-06 22:06:40 -0300

    rate limiter

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 const mongoose = require('mongoose')
 
 if (process.env.NODE_ENV !== 'prod')
@@ -16,9 +17,15 @@ mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGOD
     console.error('error in connect to MongoDB', e)
 });
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+})
+
+app.use(limiter)
 app.use(cors());
 app.use(express.json());
-app.use(routes); 
+app.use(routes);
 
 app.listen(PORT);
 console.log(`Running on ${PORT}...`);
--- routes.js
+++ routes.js
@@ -1,23 +1,54 @@
 const { Router } = require('express')
 const UserController = require('./src/Controllers/UserController')
 const TeamController = require('./src/Controllers/TeamController')
+const rateLimit = require('express-rate-limit')
 
 const routes = Router()
 
-routes.get('/api/users', UserController.index)
+const TeamLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 50,
+    message:
+        "Too many accounts created from this IP, please try again after an hour"
+})
 
-routes.get('/api/team', TeamController.index)
+const registerLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 3,
+    message:
+        "Too many accounts created from this IP, please try again after an hour"
+})
+
+const authenticateLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 30,
+    message:
+        "Too many accounts created from this IP, please try again after an hour"
+})
+
+const authorizationLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 50,
+    message:
+        "Too many accounts created from this IP, please try again after an hour"
+})
+
+// INTERNAL
+routes.get('/api/users', UserController.index)
 
 routes.get('/api/userByUsername', UserController.userByUsername)
 
 routes.get('/api/userByEmail', UserController.userByEmail)
 
-routes.post('/api/authenticate', UserController.authenticate)
-
-routes.post('/api/register', UserController.register)
-
 routes.post('/api/teamRegister', TeamController.register)
 
-routes.post('/api/authorization', UserController.authorization)
+// EXTERNAL
+routes.get('/api/team', TeamLimiter, TeamController.index)
+
+routes.post('/api/register', registerLimiter, UserController.register)
+
+routes.post('/api/authenticate', authenticateLimiter, UserController.authenticate)
+
+routes.post('/api/authorization', authorizationLimiter, UserController.authorization)
 
 module.exports = routes;
